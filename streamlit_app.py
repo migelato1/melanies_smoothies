a0750ee43c6c47f6cc4f8d79c5a5e0891725118d
migelato1 <169817001+migelato1@users.noreply.github.com>
2024-08-23 03:47:57 -0400

    Update streamlit_app.py
    updated the title of the order app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 # Write directly to the app
-st.title("Customize Your Smoothie :icecream:")
+st.title("My Parents New Healthy Diner")
 st.write(
     """Choose the fruits you want in your custom Smoothie!.
     """
